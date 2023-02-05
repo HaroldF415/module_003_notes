494d59721ab5744e5f610ba5c94fb58ae73f60e7
HaroldF415 <harold.f415@gmail.com>
2023-02-05 07:58:32 -0500

    testing 001

--- code/generatorFunctions.js
+++ code/generatorFunctions.js
@@ -1,16 +1,13 @@
 function* generatorFunction() {
-  console.log("The generator function has been called");
-  //yield 1;
-
-  // console.log("The generator function has resumed.");
-  // yield 2;
-
-  // console.log("The generator function has finished");
+  console.log("The generator function has been called.");
+  yield 1;
+  //console.log("The generator function has finished.");
 }
 
 const generatorObject = generatorFunction();
 
-console.log(genObject);
+console.log(generatorObject.next());
+console.log(generatorObject.next());
 // console.log(genObject.next());
 // console.log(genObject.next());
 // console.log(genObject.next());
