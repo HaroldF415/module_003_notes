5065fbf7741d362528b9f59339b059fa75da06e7
HaroldF415 <harold.f415@gmail.com>
2023-02-04 23:46:41 -0500

    initial commit

---
+++ code/generatorFunctions.js
@@ -0,0 +1,16 @@
+function* generatorFunction() {
+  console.log("The generator function has been called");
+  //yield 1;
+
+  // console.log("The generator function has resumed.");
+  // yield 2;
+
+  // console.log("The generator function has finished");
+}
+
+const generatorObject = generatorFunction();
+
+console.log(genObject);
+// console.log(genObject.next());
+// console.log(genObject.next());
+// console.log(genObject.next());
